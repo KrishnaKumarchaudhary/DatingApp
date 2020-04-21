4da407f9cf2174cdbee141a7d5513a08844373da
Krishna Kumar Chaudhary <ch.krishnakumar01@gmail.com>
2020-04-21 11:54:15 +0530

    initial commit

---
+++ DatingApp.Api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.Api.Models;
+
+namespace DatingApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
